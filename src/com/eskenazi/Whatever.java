1136d528b1436da048f0a86a5ae9a35c5a601a69
Dovid Eskenazi <dvdsknz@gmail.com>
2016-07-21 10:26:18 -0400

    initial commit

---
+++ src/com/eskenazi/Whatever.java
@@ -0,0 +1,14 @@
+package com.eskenazi;
+
+public class Whatever
+{
+
+    public static void main(String[] args)
+    {
+        // TODO Auto-generated method stub
+        // I do not understand parseint();
+        
+
+    }
+
+}
